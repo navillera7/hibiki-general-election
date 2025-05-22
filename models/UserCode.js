7546eb8d69406331add8f855c6401384bfcd7153
navillera7 <102697353+navillera7@users.noreply.github.com>
2025-05-21 23:34:29 -0400

    Update UserCode.js

--- models/UserCode.js
+++ models/UserCode.js
@@ -1,13 +1,19 @@
 import mongoose from 'mongoose';
 
-const userCodeSchema = new mongoose.Schema({
-  code: { type: String, unique: true },
+const UserCodeSchema = new mongoose.Schema({
+  code: { type: String, required: true, unique: true },
   voted: { type: Boolean, default: false },
   votes: {
     type: Map,
     of: Number,
     default: {},
   },
+  agendaVote: {
+    type: String,
+    enum: ['yes', 'no', 'abstain'],
+    default: null,
+  },
 });
+
 
 export default mongoose.models.UserCode || mongoose.model('UserCode', userCodeSchema);
