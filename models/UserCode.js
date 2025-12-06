3d1ebe87dfdb40c116220d7adaa3cbb2d5eca798
navillera7 <102697353+navillera7@users.noreply.github.com>
2025-12-06 10:17:17 -0500

    Update vote fields to use Korean terms

--- models/UserCode.js
+++ models/UserCode.js
@@ -10,11 +10,32 @@ const UserCodeSchema = new mongoose.Schema({
   },
   agendaVote: {
     type: String,
-    enum: ['피바', '케이비'],
+    enum: ['찬성', '반대'], // 여기 한글로 맞추기
     default: null,
   },
+  Jungvote: {
+    type: String,
+    enum: ['매우잘함', '잘함', '보통', '못함', '매우못함'],
+    default: null,
+  },
+  Leevote: {
+    type: String,
+    enum: ['매우잘함', '잘함', '보통', '못함', '매우못함'],
+    default: null,
+  },
+  JungDownvote: {
+    type: String,
+    enum: ['매우필요' , '필요', '보통', '필요없음', '전혀필요없음'],
+    default: null,  
+  },
+  Gaehunvote: {
+    type: String,
+    enum: ['매우필요' , '필요', '보통', '필요없음', '전혀필요d없음'],
+    default: null,
+  }
 });
 
 
 export default mongoose.models.UserCode || mongoose.model('UserCode', UserCodeSchema);
+
 
